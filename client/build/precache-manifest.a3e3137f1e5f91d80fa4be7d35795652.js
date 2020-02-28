3d8f584b47d7ae294dd5cc6d4a9e9496fe7a96b1
Patel, Chirag (cp121p) <cp121p@att.com>
2020-02-28 17:24:21 -0500

    first commit

---
+++ client/build/precache-manifest.a3e3137f1e5f91d80fa4be7d35795652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247ccb4bc0c71e692ee991856fc9b9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "087d1be770170cf6b092",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "4e40d7488d59026c55be",
+    "url": "/static/js/2.e9be408e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e9be408e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087d1be770170cf6b092",
+    "url": "/static/js/main.5fdf002c.chunk.js"
+  },
+  {
+    "revision": "3c7f62721c30427bb531",
+    "url": "/static/js/runtime-main.7ce61b84.js"
+  }
+]);
